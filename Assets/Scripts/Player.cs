28b2ed49dae1e522b6e74b70e205c8d084615f01
Niko Lepka <slench102+git@gmail.com>
2017-07-26 20:03:41 +0200

    add basic thruster-like movement to the PC
    PC being short for Player Character

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,13 +4,37 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    public float maxSpeed = .2f;
+    public float speedIncrease = 10f;
+
+    private Rigidbody2D _rb;
+    private Vector2 _direction;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        _rb = this.GetComponent<Rigidbody2D>();
+        print(_rb);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        var hor = Input.GetAxis("Horizontal");
+        var ver = Input.GetAxis("Vertical");
+        var dir = new Vector2(hor, ver);
+
+        ThrusterMove(dir);
 	}
+
+    /// <summary>
+    /// Describes the way movement is supposed to be performed in a given direction
+    /// This movement is independent of controls.
+    /// </summary>
+    /// <param name="direction"></param>
+    void ThrusterMove(Vector2 direction)
+    {
+        // TODO: Add speed cap
+        _rb.AddForce(direction * speedIncrease);
+    }
 }
